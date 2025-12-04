59b46952c7d94855fb0b97a8c15742bdbaaccaba
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-12-04 20:59:30 +0530

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('eureka_angular_oct_2025_project2');
+}
